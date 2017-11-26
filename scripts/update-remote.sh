a3560e067b2c59171709e6e381d02c8cee0a50eb
Céline de Roland <celinederoland@itpassion.info>
2017-11-26 06:41:07 +0100

    update config files handling

--- scripts/update-remote.sh
+++ scripts/update-remote.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 # usage : sh update-remote.sh user@remote-server path/to/delphyne/project/on/remote
 
+echo "copy ../delphyne/docker-compose.override.prod.yml to $1:$2/delphyne/docker-compose.override.yml"
 scp ../delphyne/docker-compose.override.prod.yml \
 $1:$2/delphyne/docker-compose.override.yml
+
+echo "copy ../config to $1:$2/"
+scp -r ../config/* \
+$1:$2/config
+
+ssh $1 "tree $2/config"
